846b2ba79b918d2764b31a4243ea84fb723a52a1
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-13 08:38:44 -0600

    Create chessTime.cpp

---
+++ chessTime.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Chess Time - CHESSTIME
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int min = n*60;
+        cout << min/20 << endl;
+    }
+}
