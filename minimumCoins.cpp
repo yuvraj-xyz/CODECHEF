56b667100c411cbede09d1b180f99dffdb537e69
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-13 08:42:23 -0600

    Create minimumCoins.cpp

---
+++ minimumCoins.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Minimum Coins - MINCOINSREQ
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x;
+        cin >> x;
+
+        cout << x%10 << endl;
+    }
+}
