41dd60ac9d60ac98a7bea8f7a6b0f97073685ef6
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-17 08:36:39 -0600

    Create True and False papper.cpp

---
+++ True and False papper.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// True and False Paper - TFPAPER
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n,k;
+        cin >> n >> k;
+
+        cout << n-k << endl;
+    }
+}
