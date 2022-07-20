9434c569b1d57074ef55803958a63cc623d264e4
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-20 08:41:16 -0600

    Create Passes for Fair.cpp

---
+++ Passes for Fair.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Passes for Fair
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n,k;
+        cin >> n >> k;
+
+        if(n+1<=k)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
