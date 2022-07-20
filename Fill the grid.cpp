05789f99bec24b82103f79efac60c03d849a869e
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-20 09:30:27 -0600

    Create Fill the grid.cpp

---
+++ Fill the grid.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fill the Grid - GRIDBL
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n,m;
+        cin >> n >> m;
+
+        if(n%2==0 && m%2==0)
+        {
+            cout << 0 << endl;
+        }
+        else if(n%2==0 && m%2!=0)
+        {
+            cout << n << endl;
+        }
+        else if(n%2!=0 && m%2==0)
+        {
+            cout << m << endl;
+        }
+        else
+        {
+            cout << (n+m)-1 << endl;
+        }
+    }
+}
