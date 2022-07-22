9368d1026cac83ee1ef22db21f97b63e1fc195fe
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-22 06:34:48 -0600

    Create Chief in his office.cpp

---
+++ Chief in his office.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Chief in his office - OFFICE
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x,y;
+        cin >> x >> y;
+        cout << (4*x)+y << endl;
+    }
+}
