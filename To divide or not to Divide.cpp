bd607a97060e2dd231d62067fd9454f1aa185205
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-24 12:25:21 -0600

    Create To divide or not to Divide.cpp

---
+++ To divide or not to Divide.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// To divide or not to Divide - DIVAB
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int a,b,n;
+        cin >> a >> b >> n;
+
+        if(a%b==0)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        int x=n;
+        if(x%a!=0)
+        {
+            x = n+a-(x%a);
+        }
+        while(!(x%a==0 && x%b!=0))
+        {
+            x+=a;
+            break;
+        }
+        cout << x << endl;
+    }
+}
