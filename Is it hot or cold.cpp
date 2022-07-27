b789190da8194b704176f3c0eed2bf73af9ad50f
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-07-27 08:48:39 -0600

    Create Is it hot or cold.cpp

---
+++ Is it hot or cold.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Is it hot or cold - HOTCOLD
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int c;
+        cin >> c;
+
+        if(c>20)
+        {
+            cout << "HOT" << endl;
+        }
+        else
+        {
+            cout << "COLD"  << endl;
+        }
+    }
+}
