6510b0ea0ac17bfd2e6268a59a789c1c7dd1b32a
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-08-10 08:38:40 -0600

    Create Total Population.cpp

---
+++ Total Population.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Final Population - POPULATION
+void solve(int x, int y, int z)
+{
+    cout << x-y+z << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x,y,z;
+        cin >> x >> y >> z;
+
+        solve(x,y,z);
+    }
+}
