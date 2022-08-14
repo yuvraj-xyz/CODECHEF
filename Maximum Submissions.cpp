8401a120680883034c1a2ea56e531e8bcb8d8b6e
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-08-14 01:49:01 -0600

    Create Maximum Submissions.cpp

---
+++ Maximum Submissions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Maximum Submisions - MAXIMUMSUBS
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x;
+        cin >> x;
+
+        cout << 2*x << endl;
+    }
+}
